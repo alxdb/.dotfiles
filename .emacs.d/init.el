a9232663422b368356052edab6729a1bfbf9af88
Alexander Davidson Bryan <alxdavbr@gmail.com>
2020-03-18 10:49:56 +0000

    removed emacs conf, slight vim update

--- .emacs.d/init.el
+++
@@ -1,8 +0,0 @@
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
-(unless package--initialized (package-initialize))
-
-(require 'org)
-(org-babel-load-file (concat user-emacs-directory "config.org"))
